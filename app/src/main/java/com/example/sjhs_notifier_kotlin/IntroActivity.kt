faf40796a6ddafbb3561c009305bfa412ff97191
KimWash <ckm0728wash@gmail.com>
2020-03-03 01:51:28 +0900

    인트로에서 네트워크 상태 확인

--- app/src/main/java/com/example/sjhs_notifier_kotlin/IntroActivity.kt
+++ app/src/main/java/com/example/sjhs_notifier_kotlin/IntroActivity.kt
@@ -1,9 +1,12 @@
 package com.example.sjhs_notifier_kotlin
 
 import android.app.Activity;
+import android.content.Context
 import  android.content.Intent;
+import android.net.ConnectivityManager
 import android.os.Bundle;
 import android.os.Handler;
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -16,14 +19,35 @@ public class IntroActivity : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        val hander = Handler()
-        hander.postDelayed({
-            startActivity(Intent(application, MainActivity::class.java))
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        val status = networkInfo != null && networkInfo.isConnected
+        if (status == false) {
+            val alert_confirm =
+                AlertDialog.Builder(this)
+            alert_confirm.setMessage("인터넷에 연결되어있지 않습니다. 확인 후 다시 이용 바랍니다.").setCancelable(false)
+                .setPositiveButton(
+                    "확인"
+                ) { dialog, which ->
+                    // 'YES'
+                    android.os.Process.killProcess(android.os.Process.myPid())
+                }
+            val alert = alert_confirm.create()
+            alert.show()
+        }
+        else{
+            val hander = Handler()
+            hander.postDelayed({
+                startActivity(Intent(application, MainActivity::class.java))
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
 
-            this@IntroActivity.finish()
+                this@IntroActivity.finish()
 
-        }, SPLASH_TIME.toLong())
+            }, SPLASH_TIME.toLong())
+        }
+
+
+
     }
 
 
